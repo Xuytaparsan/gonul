0806ed239de7a6c595a9c9911b49b40b48588b98
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2022-04-29 00:13:46 +0400

    update DemonMusicBot.py

--- DemonMusicBot.py
+++ DemonMusicBot.py
@@ -28,7 +28,7 @@ bot = Client(
 
 @bot.on_message(filters.command(['start']))
 def start(client, message):
-    demon = f'👋 **Salam** {message.from_user.mention}\n\n**ℹ️ Mən musiqi yükləmə botuyam, istədiyiniz musiqini yükləyə bilərəm**\n\n** ✅ Yardım üçün** /help **əmrindən** istifadə edin
+    demon = f'👋 Salam {message.from_user.mention}\n\nℹ️ Mən musiqi yükləmə botuyam, istədiyiniz musiqini yükləyə bilərəm\n\n ✅ Yardım üçün /help  Yazın
     message.reply_text(
         text=demon, 
         quote=False,
